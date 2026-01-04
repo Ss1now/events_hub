ffc55c7aeae4c96baed964f73ec4419b7424a8e1
Howard Zhao <howardzhao06@gmail.com>
2026-01-04 16:28:53 -0600

    create sorting feature

--- components/bloglist.jsx
+++ components/bloglist.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { blog_data } from '@/assets/assets';
 import BlogItem from './blogitem';
+import { useState } from 'react';
 
 const BlogList = () => {
+
+        const [menu, setMenu] = useState("All");
+
     return (
         <div>
             <div className='flex justify-center gap-6 my-10'>
-                <button className='bg-black text-white py-1 px-4 rounded-sm'>All</button>
-                <button>Design</button>
-                <button>Development</button>
-                <button>Tutorial</button>
+                <button onClick={()=>setMenu('All')} className={menu==="All"?'bg-black text-white py-1 px-4 rounded-sm':""}>All</button>
+                <button onClick={()=>setMenu('Design')} className={menu==="Design"?'bg-black text-white py-1 px-4 rounded-sm':""}>Design</button>
+                <button onClick={()=>setMenu('Development')} className={menu==="Development"?'bg-black text-white py-1 px-4 rounded-sm':""}>Development</button>
+                <button onClick={()=>setMenu('Tutorial')} className={menu==="Tutorial"?'bg-black text-white py-1 px-4 rounded-sm':""}>Tutorial</button>
             </div>
             <div className='flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24'>
-                {blog_data.map((item,index)=>{
+                {blog_data.filter((item)=> menu==="All"?true:item.category===menu).map((item,index)=>{
                     return <BlogItem key={index} image={item.image} title={item.title} description={item.description} category={item.category} />
                 })}
             </div>
