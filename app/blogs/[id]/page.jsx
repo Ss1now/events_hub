f62a470334871ac89cf35717834377eeb3f589a7
Howard Zhao <howardzhao06@gmail.com>
2026-01-05 13:53:34 -0600

    not final version of params

--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -1,10 +1,41 @@
+'use client'
 import React from 'react'
+import { useEffect, useState } from 'react';
+import { blog_data } from '@/assets/assets';
+import { useParams } from 'next/navigation';
+import Image from 'next/image';
+import { assets } from '@/assets/assets';
+
 
 const page = ({params}) => {
-    return (
-        <div>
-            {params.id}
+
+    const [data,setData] = useState(null);
+
+    const fetchBlogData = () => {
+        for(let i=0;i<blog_data.length;i++){
+            if (Number(params.id)===blog_data[i].id){
+                setData(blog_data[i]);
+                console.log(blog_data[i]);
+                break;
+            }
+        }
+    }   
+
+    useEffect(() => {
+        fetchBlogData();
+    }, []);
+
+    return (data?<>
+        <div className ='bg-gray-200 py-5 px-5 md:px-12 lg:px-28'>
+            <div className='flex justify-between items-center'>
+            <Image src={assets.logo} width={180} alt='' className= 'w-[130px] sm:w-auto'/>
+            <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-black'>Get Started</button>
+            </div>
+            <div className='text-center my-24'>
+                <h1>{data.title}</h1>
+            </div>
         </div>
+        </>:<></>
     )
 }
 
--- components/blogitem.jsx
+++ components/blogitem.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import Image from 'next/image';
+import { assets, blog_data } from '@/assets/assets';
+import Link from 'next/link';
+import Page from '@/app/blogs/[id]/page';
 
-const BlogItem = ({title, description, category, image, author, date}) => {
+const BlogItem = ({title, description, category, image, id}) => {
     return (
         <div className='max-w-[330px] sm:max-w-[300px] bg-white border border-black'>
-            <Image src={image} width={300} alt={title} height={400} className='border-b border-black'/>
+            <Link href={`/blogs/${id}`}>
+            <Image src={image} width={300} alt='' height={400} className='border-b border-black'/>
+            </Link>
             <p className='ml-5 mt-5 px-1 inline-block bg-black text-white text-sm'>{category}</p>
             <div className="p-5">
                 <h5 className='mb-2 text-lg font-medium tracking-tight text-gray-900'>{title}</h5>
                 <p className='mb-3 text-sm tracking-tight text-gray-700'>{description}</p>
-                <div className='text-xs text-gray-600 mb-3 flex justify-between'>
-                    <span>{author}</span>
-                    <span>{new Date(date).toLocaleDateString()}</span>
-                </div>
-                <div className='inline-flex items-center py-2 font-semibold text-center'>
+                <Link href={`/blogs/${id}`} className='inline-flex items-center py-2 font-semibold text-center'>
                     Read more
-                </div>
+                </Link>
             </div>
         </div>
     )
